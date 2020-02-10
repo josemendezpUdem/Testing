e36300ca4c2bca0a764e00832af55fccb84c7318
josemendezpUdem <57606793+josemendezpUdem@users.noreply.github.com>
2020-02-10 15:32:44 -0600

    Arreglar menu con un while

--- Principal.java
+++ Principal.java
@@ -9,28 +9,32 @@ public class Principal
    public static void menu()
    {
       System.out.println("Inicio");
-
-      System.out.println("[1]\tSumar");
-      System.out.println("[2]\tRestar");
-      System.out.println("[3]\tMultiplicar");
-      System.out.println("[4]\tDividir");
-      
-      int opcion = Lectura.readInt();
-      
-      switch(opcion)
+      int opcion = 0;
+      do
       {
-         case 1:
-            System.out.println("Sumar");
-            break;
-         case 2:
-            System.out.println("Restar");
-            break;
-         case 3:
-            System.out.println("Multiplicar");
-            break;
-         case 4:
-            System.out.println("Dividir");
-            break;
+         System.out.println("[1]\tSumar");
+         System.out.println("[2]\tRestar");
+         System.out.println("[3]\tMultiplicar");
+         System.out.println("[4]\tDividir");
+         
+         opcion = Lectura.readInt();
+         
+         switch(opcion)
+         {
+            case 1:
+               System.out.println("Sumar");
+               break;
+            case 2:
+               System.out.println("Restar");
+               break;
+            case 3:
+               System.out.println("Multiplicar");
+               break;
+            case 4:
+               System.out.println("Dividir");
+               break;
+         }
       }
+      while(opcion <= 0 || opcion >= 5);    
    }
 }
