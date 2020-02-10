c68e0347c9fe42e764c85e9f3daab28ac73ac91b
josemendezpUdem <57606793+josemendezpUdem@users.noreply.github.com>
2020-02-10 15:15:49 -0600

    Inicio

--- Principal.java
+++ Principal.java
@@ -3,17 +3,6 @@ public class Principal
 {
 	public static void main(String[] args)
    {
-      //Guardo el objeto
-      Circulo circulo = new Circulo(5);
-      SaveObjects.saveObject(circulo, "circulo.data");
-      
-      
-      //Consigo el objeto
-      Circulo circulo2 = (Circulo)SaveObjects.getObject("circulo.data");
-      System.out.println(circulo2);
-
-      System.out.println("Adios raza");
-      System.out.println("Hola Enrico");
-      System.out.println("Hola, Pablo");
+      System.out.println("Inicio");
 	}
 }
